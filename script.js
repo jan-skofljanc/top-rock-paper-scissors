b508848f7d5b532cf3be6c50f5caba26d0524433
jan-skofljanc <42697383+jan-skofljanc@users.noreply.github.com>
2026-02-24 19:44:15 +0100

    basic logic to get choice from computer

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("test text")
+console.log("test text");
+
+function getComputerChoice() {
+    let a = Math.random();
+    console.log(a);
+    if (a <= 0.33) {
+        console.log("Computer chose: ROCK");
+    } else if (a > 0.33 && a <= 0.66) {
+        console.log("Computer chose: PAPER");
+    } else {
+        console.log("Computer chose: SCISSORS");
+    }
+}
+
+getComputerChoice();
+
+function getHumanChoice() {
+    
+}
